9b1a7b22b5146e27698372822c7a032b0794cc57
Hesaam Farhang <hesaam@2600hz.com>
2020-03-24 09:46:05 -0700

    use couchdb3

--- docker/scripts/create_couchdb_dbs.sh
+++ docker/scripts/create_couchdb_dbs.sh
@@ -5,7 +5,7 @@ COUCH_PORT="${2:-5984}"
 
 echo ":: Intializing database for http://$COUCH_IP:$COUCH_PORT"
 
-curl -iX PUT "http://$COUCH_IP:$COUCH_PORT/_global_changes"
-curl -iX PUT "http://$COUCH_IP:$COUCH_PORT/_replicator"
-curl -iX PUT "http://$COUCH_IP:$COUCH_PORT/_metadata"
-curl -iX PUT "http://$COUCH_IP:$COUCH_PORT/_users"
+curl -iX PUT "http://admin:mysecurepassword@$COUCH_IP:$COUCH_PORT/_global_changes"
+curl -iX PUT "http://admin:mysecurepassword@$COUCH_IP:$COUCH_PORT/_replicator"
+curl -iX PUT "http://admin:mysecurepassword@$COUCH_IP:$COUCH_PORT/_metadata"
+curl -iX PUT "http://admin:mysecurepassword@$COUCH_IP:$COUCH_PORT/_users"
