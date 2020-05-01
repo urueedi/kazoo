0a495d0a1f7147cfb5cca4aaa09e2bf7a8f932ca
Hesaam Farhang <hesaam@2600hz.com>
2020-05-01 12:10:28 -0700

    use admin:admin as pass for couchd, fic monster entry point

--- docker/files/monster-entrypoint.sh
+++ docker/files/monster-entrypoint.sh
@@ -1,9 +1,14 @@
 #!/bin/sh
 
-echo "Setting up Monster-UI"
-[ ! -d /opt/monster-ui/node_modules ] || npm install
-
 cd /opt/monster-ui
+
+echo "Setting up Monster-UI"
+if [ ! -d /opt/monster-ui/node_modules ]; then
+    npm install
+else
+    echo "no need for npm install"
+    ls -l /opt/monster-ui/
+fi
 
 echo "Executing command: $@"
 exec "$@"
